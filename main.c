457906128dd8e9a7e06103d70fce87683a64354c
Bomber Barbara <Aach06@gmail.com>
2020-11-16 11:28:45 -0600

    V.2 is out, featuring button input.

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <pspdisplay.h>
+#include <pspctrl.h>
 
 #include "C:/Users/Aach0/Documents/GitHub/PSP-WiFi-Controller/common/callback.h"
 
-#define VERS 1 //Talk about this
-#define REVS 0
+#define VERS 1 
+#define REVS 1
 
-PSP_MODULE_INFO("Hello World", PSP_MODULE_USER, VERS, REVS);
+PSP_MODULE_INFO("PSP Wifi Controller", PSP_MODULE_USER, VERS, REVS);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER); 
 PSP_HEAP_SIZE_MAX();
 
@@ -18,11 +19,43 @@ int main(void)
 	pspDebugScreenInit();
 	setupExitCallback();
 
-	while(isRunning())
+	int running = isRunning();
+
+	SceCtrlData buttonInput;
+
+	sceCtrlSetSamplingCycle(0);
+	sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
+
+	while(running)
 	{
+		running = isRunning();
+
 		pspDebugScreenSetXY(0, 0);
-		printf("If you can read this, you are running my unsigned software on your PSP. Congrats, criminal scum.");
-		sceDisplayWaitVblankStart();
+		printf("Analog X = %d ", buttonInput.Lx); 
+		printf("Analog Y = %d \n", buttonInput.Ly);
+
+		sceCtrlPeekBufferPositive(&buttonInput, 1); 
+
+		if(buttonInput.Buttons != 0) 
+		{ 
+			if(buttonInput.Buttons & PSP_CTRL_START){ 
+									printf("Start"); 
+									running = 0;
+			} 
+			if(buttonInput.Buttons & PSP_CTRL_SELECT)	printf("Select");
+			if(buttonInput.Buttons & PSP_CTRL_UP)		printf("Up"); 
+			if(buttonInput.Buttons & PSP_CTRL_DOWN)		printf("Down"); 
+			if(buttonInput.Buttons & PSP_CTRL_RIGHT)	printf("Right"); 
+			if(buttonInput.Buttons & PSP_CTRL_LEFT)		printf("Left"); 
+
+			if(buttonInput.Buttons & PSP_CTRL_CROSS)	printf("Cross"); 
+			if(buttonInput.Buttons & PSP_CTRL_CIRCLE)	printf("Circle"); 
+			if(buttonInput.Buttons & PSP_CTRL_SQUARE)	printf("Square"); 
+			if(buttonInput.Buttons & PSP_CTRL_TRIANGLE)	printf("Triangle"); 
+	
+			if(buttonInput.Buttons & PSP_CTRL_RTRIGGER)	printf("R-Trigger");
+			if(buttonInput.Buttons & PSP_CTRL_LTRIGGER)	printf("L-Trigger");
+		} 
 	}
 
 	sceKernelExitGame();	
